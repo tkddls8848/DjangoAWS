2cd4626a02e9d86f9f5efca0584551cf502b8952
PSI <tkddls8848@gmail.com>
2020-12-13 19:36:36 +0900

    add allowed host

--- DjangoAWS/DjangoAWS/settings.py
+++ DjangoAWS/DjangoAWS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=-!f@4(z19_c4^i8cy1a4t-9h4+u5c097rtp3gi$m=q_qx-ab1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.164.66.139']
 
 
 # Application definition
